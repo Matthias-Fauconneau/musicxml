773d6f53a33f759d74fa959667f7bee8538006cc
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-06-18 11:20:40 +0200

    ~serde/quick-xml with roxmltree

--- src/xml.rs
+++ src/xml.rs
@@ -1,9 +1,4 @@
-#![allow(unreachable_code)]
-pub trait OptionExt<T> { fn try_map<E, U, F:FnOnce(T)->Result<U, E>>(self, f: F) -> Result<Option<U>, E>; }
-impl<T> OptionExt<T> for Option<T> {
-	fn try_map<E, U, F:FnOnce(T) ->Result<U, E>>(self, f: F) -> Result<Option<U>, E> { self.map(f).transpose() }
-}
-
+//#![allow(unreachable_code)]
 mod error {
 #[derive(Debug)] pub struct Error(anyhow::Error);
 impl std::fmt::Display for Error { fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result { self.0.fmt(f) } }
@@ -13,18 +8,25 @@ impl serde::de::Error for Error { fn custom<T: std::fmt::Display>(msg: T) -> Sel
 
 /// ~serde/quick-xml with roxmltree
 
-pub struct Deserializer<'t, 'input> {
-	node: roxmltree::Node<'t, 'input>,
+pub struct Deserializer<'de> {
+	attributes: std::iter::Peekable<std::slice::Iter<'de, roxmltree::Attribute<'de>>>,
+	children: std::iter::Peekable<roxmltree::Children<'de, 'de>>
 }
 
-use {fehler::*, serde::de::{self, Error, Visitor, Deserializer as deserialize, DeserializeSeed, IntoDeserializer}};
+impl<'de> Deserializer<'de> {
+	fn new(node: roxmltree::Node<'de, 'de>) -> Self { Self{attributes: node.attributes().iter().peekable(), children: node.children().peekable()} }
+}
 
-impl<'de, 't> serde::Deserializer<'de> for /*&'t mut*/ Deserializer<'t, 'de> {
+use {fehler::*, serde::de::{self, Visitor, Deserializer as deserialize, DeserializeSeed, IntoDeserializer, Error as invalid_type}};
+
+impl<'t, 'de> serde::Deserializer<'de> for &'t mut Deserializer<'de> {
 	type Error = error::Error;
 
 	fn deserialize_any<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value,Self::Error> {
-		throw!(Self::Error::invalid_type(de::Unexpected::Other("unimplemented"), &visitor))
+		println!("{}", &visitor as &dyn de::Expected);
+		self.deserialize_struct("", &[], visitor)
 	}
+
 	serde::forward_to_deserialize_any!{
 		char bytes byte_buf str string identifier bool
 		u8 u16 u32 u64 u128 i8 i16 i32 i64 i128 f32 f64
@@ -32,78 +34,55 @@ impl<'de, 't> serde::Deserializer<'de> for /*&'t mut*/ Deserializer<'t, 'de> {
 	}
 
 	#[throws(Self::Error)] fn deserialize_seq<V: Visitor<'de>>(self, visitor: V) -> V::Value {
-		struct Seq<'t, 'de: 't>(/*&'t mut*/ Deserializer<'t, 'de>);
-		impl<'de, 't> de::SeqAccess<'de> for Seq<'t, 'de> {
+		struct Seq<'t, 'de: 't>{name: Option<&'t str>, iter: &'t mut std::iter::Peekable<roxmltree::Children<'de, 'de>>}
+		impl<'t, 'de> de::SeqAccess<'de> for Seq<'t, 'de> {
 			type Error = error::Error;
-			#[throws(Self::Error)] fn next_element_seed<T: DeserializeSeed<'de>>(&mut self, _seed: T) -> Option<T::Value> {
-				panic!("next_element_seed") //seed.deserialize(&mut *self.0)?
+			#[throws(Self::Error)] fn next_element_seed<T: DeserializeSeed<'de>>(&mut self, seed: T) -> Option<T::Value> {
+				if let Some(child) = self.iter.peek() { if self.name.map_or(true, |name| child.tag_name().name() == name) {
+					Some(seed.deserialize(&mut Deserializer::new(self.iter.next().unwrap()))?)
+				} else { None } } else { None }
 			}
 		}
-		visitor.visit_seq(Seq(/*&mut **/self))?
+		visitor.visit_seq(Seq{name: self.children.peek().map(|c| c.tag_name().name()), iter: &mut self.children})?
 	}
 
-	#[throws(Self::Error)]
-	fn deserialize_struct<V: Visitor<'de>>(self, _name: &'static str, _fields: &'static [&'static str], visitor: V) -> V::Value { //Result<V::Value,Error> {
-		enum Value<'t, 'input: 't> { Attribute(&'t str), Descendant(roxmltree::Node<'t, 'input>) }
-		/*impl<'de, 't> std::ops::Deref for Value<'t, 'de> {
-			type Target = dyn IntoDeserializer<'de, error::Error, Deserializer = &'t mut self::Deserializer<'t, 'de, error::Error>>;
-			fn deref(&self) -> &Self::Target {
-				match self {
-					Attribute(str) => str,
-					Descendant(node) => node
-				}
-			}
-		}*/
-		/*impl<'de, 't> IntoDeserializer<'de, error::Error> for Value<'t, 'de> {
-			type Deserializer = /*&'t mut*/ self::Deserializer<'t, 'de, error::Error>;
-			fn into_deserializer(self) -> Self::Deserializer { match self {
-				Attribute(str) => str.into_deserializer(),
-				Descendant(node) => node.into_deserializer()
-			} }
-		}*/
-		struct MapAccess<'t, 'input, I> {
-			iter: I,
-			next_value: Option<Value<'t, 'input>>,
-		}
-		impl<'de, 't, I:Iterator<Item=(&'t str, Value<'t, 'de>)>> de::MapAccess<'de> for MapAccess<'t, 'de, I> {
+	#[throws(Self::Error)] fn deserialize_struct<V: Visitor<'de>>(self, _name: &'static str, _fields: &'static [&'static str], visitor: V) -> V::Value {
+		println!("struct {} {:?}", _name, _fields);
+		impl<'t, 'de> de::MapAccess<'de> for Deserializer<'de> {
 			type Error = error::Error;
 
 			#[throws(error::Error)] fn next_key_seed<K: DeserializeSeed<'de>>(&mut self, seed: K) -> Option<K::Value> {
-				self.iter.next().try_map(/*#[throws]*/|(name, value)| {
-					self.next_value = Some(value);
-					Ok(seed.deserialize(<&str as IntoDeserializer<Self::Error>>::into_deserializer(name))?)
-				})?
+				if let Some(a) = self.attributes.peek() {
+					Some(seed.deserialize(<&str as IntoDeserializer<Self::Error>>::into_deserializer(a.name()))?)
+				} else
+				if let Some(c) = self.children.peek() {
+					Some(seed.deserialize(<&str as IntoDeserializer<Self::Error>>::into_deserializer(c.tag_name().name()))?)
+				} else
+				{ None }
 			}
-			#[throws(error::Error)] fn next_value_seed<V: DeserializeSeed<'de>>(&mut self, seed: V) -> V::Value {
-				//seed.deserialize(<Value as IntoDeserializer<Self::Error>>::into_deserializer(self.next_value.unwrap()))?
-				match self.next_value.take().unwrap() {
-					Attribute(str) => seed.deserialize(<&str as IntoDeserializer<Self::Error>>::into_deserializer(str)),
-					Descendant(node) => seed.deserialize(Deserializer{node}),
-				}?
+			#[throws(Self::Error)] fn next_value_seed<V: DeserializeSeed<'de>>(&mut self, seed: V) -> V::Value {
+				if let Some(a) = self.attributes.next() {
+					seed.deserialize(<&str as IntoDeserializer<Self::Error>>::into_deserializer(a.value()))?
+				} else
+				{ seed.deserialize(&mut *self)? }
+				//{ throw!(Self::Error::custom("Missing value for key")) }
 			}
 		}
-		use Value::*;
-		visitor.visit_map(MapAccess{
-			iter: 				self.node.attributes().iter().map(|attribute| (attribute.name(), Attribute(attribute.value())))
-					.chain( self.node.descendants().map(|descendant| (descendant.tag_name().name(), Descendant(descendant))) ),
-			next_value: None,
-		})?
+		let child = self.children.next().ok_or_else(|| Self::Error::invalid_type(de::Unexpected::Other("End"), &visitor))?;
+		visitor.visit_map(Deserializer::new(child))?
     }
 
 	#[throws(Self::Error)] fn deserialize_enum<V: Visitor<'de>>(self, _name: &'static str, _variants: &'static [&'static str], visitor: V) -> V::Value {
-		struct Enum<'t, 'de: 't>(Deserializer<'t, 'de>);
-		impl<'de, 't> de::EnumAccess<'de> for Enum<'t, 'de> {
+		struct Enum<'t, 'de>(&'t mut Deserializer<'de>);
+		impl<'t, 'de> de::EnumAccess<'de> for Enum<'t, 'de> {
 			type Error = error::Error;
 			type Variant = Self;
-			#[throws(Self::Error)] fn variant_seed<V: DeserializeSeed<'de>>(self, _seed: V) -> (V::Value, Self::Variant) {
-				unimplemented!() //(seed.deserialize(&mut *self.0)?, self)
-			}
+			#[throws(Self::Error)] fn variant_seed<V: DeserializeSeed<'de>>(self, seed: V) -> (V::Value, Self::Variant) { (seed.deserialize(&mut *self.0)?, self) }
 		}
-		impl<'de, 'a> de::VariantAccess<'de> for Enum<'a, 'de> {
+		impl<'t, 'de> de::VariantAccess<'de> for Enum<'t, 'de> {
 			type Error = error::Error;
-			//#[allow(unreachable_code)]
 			#[throws(Self::Error)] fn unit_variant(self) {}
-			#[throws(Self::Error)] fn newtype_variant_seed<T: DeserializeSeed<'de>>(self, seed: T) -> T::Value { seed.deserialize(self.0)? }
+			#[throws(Self::Error)] fn newtype_variant_seed<T: DeserializeSeed<'de>>(self, seed: T) -> T::Value { seed.deserialize(&mut *self.0)? }
 			#[throws(Self::Error)] fn tuple_variant<V: Visitor<'de>>(self, _len: usize, visitor: V) -> V::Value { self.0.deserialize_seq(visitor)? }
 			#[throws(Self::Error)] fn struct_variant<V: Visitor<'de>>(self, _fields: &'static [&'static str], visitor: V) -> V::Value { self.0.deserialize_map(visitor)? }
 		}
@@ -134,6 +113,10 @@ impl<'de, 't> serde::Deserializer<'de> for /*&'t mut*/ Deserializer<'t, 'de> {
 	#[throws] fn deserialize_newtype_struct<V:Visitor<'de>>(self, _name: &'static str, visitor: V) -> V::Value { visitor.visit_newtype_struct(self)? }*/
 }
 
-#[throws(anyhow::Error)] pub fn from_bytes<'t, T: serde::Deserialize<'t>>(bytes: &'t [u8]) -> T {
-	T::deserialize(Deserializer{node: roxmltree::Document::parse(std::str::from_utf8(&bytes)?)?.root_element()})?
+#[throws(anyhow::Error)] pub fn from_node<'input: 'de, 't: 'de, 'de, T: serde::Deserialize<'de>>(node: roxmltree::Node<'t, 'input>) -> T {
+	T::deserialize(&mut Deserializer::new(node))?
 }
+#[throws(anyhow::Error)] pub fn from_document<'input: 'de, 'de, T: serde::Deserialize<'de>>(document: &'de roxmltree::Document<'input>) -> T {
+	from_node(document.root())?
+}
+#[throws(anyhow::Error)] pub fn parse(bytes: &[u8]) -> roxmltree::Document { roxmltree::Document::parse(std::str::from_utf8(bytes)?)? }
