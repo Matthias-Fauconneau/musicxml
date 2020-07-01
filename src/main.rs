ff245c60ef39b88b03d75b8e3268f85caadcf808
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-07-02 00:46:46 +0200

    Defer scaling

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,8 @@ lazy_static::lazy_static! {
 	static ref font: framework::font::File<'static> = framework::font::open(std::path::Path::new("/usr/local/share/fonts/bravura/Bravura.otf")).unwrap();
 }
 
-fn layout(music: &MusicXML, width: u32) -> Graphic<'static> {
-	let scale = Ratio{num: 360, div: font.units_per_em().unwrap() as u32};
-	let staff_height = scale.div;
+fn layout(music: &MusicXML, width: i32) -> Graphic<'static> {
+	let staff_height = font.units_per_em().unwrap() as u32;
 	let interval = staff_height / 4; // 90
 	let staff_distance = 9*interval;
 
@@ -35,7 +34,8 @@ fn layout(music: &MusicXML, width: u32) -> Graphic<'static> {
 	let mut fill = Vec::new();
 	for (staff, _) in staves.iter().enumerate() {
 		for step in (-8..=0).step_by(2) {
-			fill.push(Rect{top_left: scale * xy{x:0, y: y(staff, step)}, size: xy{x: width, y: 1}});
+			let y = y(staff, step);
+			fill.push(Rect{top_left: xy{x:0, y}, bottom_right: xy{x: width, y: y+1}});
 		}
 	}
 
@@ -43,10 +43,10 @@ fn layout(music: &MusicXML, width: u32) -> Graphic<'static> {
 	for part in &music.score_partwise.parts {
 		for measure in &part.measures {
 			for music_data in &measure.music_data {
-				let x = glyph.last().map(|g:&Glyph| (g.top_left+font.size(scale, g.id).into()).x).unwrap_or(0);
+				let x = glyph.last().map(|g:&Glyph| (g.top_left+font.size(g.id).into()).x).unwrap_or(0);
 				let mut glyph = |staff, step, id| {
 					let id = font.glyph_index(id).unwrap();
-					glyph.push(Glyph{top_left: scale * xy{
+					glyph.push(Glyph{top_left: xy{
 						x: x + font.glyph_hor_side_bearing(id).unwrap() as i32,
 						y: y(staff, step) - font.glyph_bounding_box(id).unwrap().y_max as i32,
 					}, id})
@@ -72,7 +72,10 @@ fn layout(music: &MusicXML, width: u32) -> Graphic<'static> {
 			}
 		}
 	}
-	Graphic{fill, font: &font, scale, glyph}
+	Graphic{scale: Ratio{num: 360, div: staff_height}, fill, font: &font, glyph}
 }
 
-#[throws] fn main() { window::run(&mut GraphicView::new(layout(&xml::from_document(&xml::parse(&std::fs::read("../test.xml")?)?)?, u32::MAX)))? }
+#[throws] fn main() {
+	framework::rstack_self()?; framework::sigint_trace();
+	window::run(&mut GraphicView::new(layout(&xml::from_document(&xml::parse(&std::fs::read("../test.xml")?)?)?, i32::MAX)))?
+}
