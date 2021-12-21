341888793f20d3b3f3d1ef553eacd2a3c4c1e831
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2021-12-21 21:04:28 +0100

    stub: Metronome, Wedge, OctaveShift

--- src/direction.rs
+++ src/direction.rs
@@ -1,5 +1,5 @@
-use {std::{path::Path,env::var}, xy::xy, fehler::throws, crate::Error,
-	crate::music_xml::{Font, PrintStyle, Direction, DirectionType, DirectionTypeData, Dynamics}, 
+use {xy::xy, fehler::throws, crate::Error,
+	crate::music_xml::{Font, PrintStyle, Direction, DirectionType, DirectionTypeData, Dynamics, Metronome, Wedge, OctaveShift}, 
 	crate::{measure::MeasureLayoutContext, staff::Staff}};
 impl MeasureLayoutContext<'_> { 
 	#[throws] pub fn direction(&mut self, _staves: &mut [Staff], Direction{direction_type, ..}: &Direction) {
@@ -9,8 +9,10 @@ impl MeasureLayoutContext<'_> {
 					let text = format!("{text:?}");
 					let font = self.measure.sheet.font;
 					let face = if let Some((_,face)) = font.iter().find(|(key,_)| key==face) { face } else {
+						/*use std::{path::Path,env::var}
 						font.push((face.clone(), ui::font::open(Path::new(&(var("HOME")?+"/.local/share/fonts/BravuraText.otf")))?));
-						&font.iter().find(|(key,_)| key==face).unwrap().1
+						&font.iter().find(|(key,_)| key==face).unwrap().1*/
+						ui::text::default_font()[0]
 					};
 					use ui::{graphic, text::{Plain, View, layout, Glyph, unicode_segmentation::UnicodeSegmentation}};
 					let text = View::new_with_face(&face, Plain(text));
@@ -20,6 +22,13 @@ impl MeasureLayoutContext<'_> {
 							y: (self.sheet.staff_height+self.staff_distance/2) as i32 - face.glyph_bounding_box(id).unwrap().y_max as i32,
 						}, face, id});
 					}
+				},
+				DirectionTypeData::Metronome(Metronome{..}) => {
+
+				},
+				DirectionTypeData::Wedge(Wedge{..}) => {
+				},
+				DirectionTypeData::OctaveShift(OctaveShift{..}) => {
 				},
 				_ => panic!("{direction:?}")
 			}}	
